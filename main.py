815827ea9912a0fb1a932c2fc21292c42771db29
Zajancz <seledka.artem123@gmail.com>
2023-05-31 16:42:30 +0200

    [#1] main.py : healthy_fovmask : Optimized Canny Edge Detector Performance with Green Channel and Image Masking

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import cv2
 import copy
 import matplotlib
 import numpy as np
-from skimage import filters
+from skimage import io, filters
 from matplotlib import pyplot as plt
 
 def main():
-    image = cv2.imread('images/oko2.png')
-    # image = cv2.imread('images/11_h.jpg')
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    #image = io.imread('images/oko2.png')
+    image = cv2.imread('images/01_h.jpg')  # Read the image in grayscale
+    mask = cv2.imread('healthy_fovmask/01_h_mask.tif')
 
-    blurred_image = cv2.GaussianBlur(image, (5, 5), 2)
+    # image = io.imread('images/11_h.jpg')
+    green_channel_image = image[:, :, 1]  # Select the green channel (index 1)
+    green_channel_mask = mask[:, :, 1]
+
+    image_copy = copy.deepcopy(green_channel_image)
+
+    masked_image = cv2.bitwise_and(image_copy, image_copy, mask=green_channel_mask)
+
+
+
+    blurred_image = cv2.GaussianBlur(masked_image, (5, 5), 2)
     edges = cv2.Canny(blurred_image, 5, 18)
+
+
     negative_image = 255 - edges
 
-    # Display the negative image
+    # # Display the negative image
     plt.imshow(negative_image, cmap='gray')
     plt.title('Negative Image')
     plt.axis('off')
@@ -23,4 +35,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
